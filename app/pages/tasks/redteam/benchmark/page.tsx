eb80125516b34e70a6aad4c240f06d9ecf808f9d
Kellogs <a.chelini99@gmail.com>
2025-10-16 19:00:08 +0200

    starting the definition of the Store for the Titan FrontEnd application

--- app/pages/tasks/redteam/benchmark/page.tsx
+++ app/pages/tasks/redteam/benchmark/page.tsx
@@ -5,6 +5,7 @@ import { RegisterObjectProps } from '@/interfaces/NNInterfaces';
 import './Benchmark.css';
 import TableWrapper from "./TableWrapper"
 import { Play, Settings, Ruler, BrickWallFire, Bug, Gauge } from 'lucide-react';
+import useNNTrustStore from '@/store/nnTrustStore';
 
 const Benchmark: React.FC = () => {
   const [attacks, setAttacks] = useState<RegisterObjectProps[]>([]);
@@ -12,7 +13,6 @@ const Benchmark: React.FC = () => {
 
   const [selectedAttacks, setSelectedAttacks] = useState<Set<string>>(new Set());
   const [selectedMetrics, setSelectedMetrics] = useState<Set<string>>(new Set());
-  const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
     async function fetchItem() {
@@ -25,11 +25,11 @@ const Benchmark: React.FC = () => {
         const json = await response.json();
         setAttacks(json);
         if (json.length > 0) {
-          const allIds = json.map((attack: AttackProps) => attack.id);
+          const allIds = json.map((attack: RegisterObjectProps) => attack.id);
           setSelectedAttacks(new Set(allIds));
         }
       } catch (err) {
-        setError(err instanceof Error ? err.message : "An error occurred");
+        console.log(err instanceof Error ? err.message : "An error occurred");
       }
       // fetching the metrics
       try {
@@ -44,99 +44,91 @@ const Benchmark: React.FC = () => {
           setSelectedMetrics(new Set(allIds));
         }
       } catch (err) {
-        setError(err instanceof Error ? err.message : "An error occurred");
+        console.log(err instanceof Error ? err.message : "An error occurred");
       }
     }
     fetchItem();
   }, []);
 
+  // variables for executing the benchmarking
+  const [executeBenchmark, setExecuteBenchmark] = useState<boolean>(true)
+  const setStoredAttacks = useNNTrustStore((state) => state.setAttacks)
+  const ableMonitoring = useNNTrustStore((state) => state.setEnableMonitoring)
 
-
-
-
-  function errorPage() {
-    // handle the page if there are some problems during the loading of the attacks.
-    return (
-      <div className="attack-list">
-        <div className="flex items-center justify-center min-h-screen">
-          <div className="text-red-200 text-xl">Error: {error}</div>
+  const storedAttacks = useNNTrustStore((state) => state.attacks)
+  // True display of the page when everything goes as planned.
+  return (
+    < div className="attack-list" >
+      <div className='attack-title'>
+        <div className='attack-header'>
+          <div className='attack-icon'>
+            <BrickWallFire size={'6vw'} color='red' />
+            <h1>Red Teaming</h1>
+          </div>
+          <p style={{ margin: '0' }}>This page provides a set of vulnerabilities that can be used to test the model's robustness and a set of metrics to register the performance of each attack.</p>
         </div>
+        <button
+          className='attack-button'
+          disabled={!executeBenchmark}
+          onClick={() => {
+            //Since there must be only one benchmark at the time I have to disable the button at the beginning
+            setExecuteBenchmark(false);
+            setStoredAttacks(selectedAttacks)
+            ableMonitoring(true)
+            console.log(storedAttacks)
+
+            setExecuteBenchmark(true)
+          }}>
+          <Play className='icon' />
+          <div className='btn-desc'> Execute benchmark</div>
+        </button>
       </div>
-    );
-  }
 
-  function attackPage() {
-    // True display of the page when everything goes as planned.
-    return (
-      < div className="attack-list" >
-        <div className='attack-title'>
-          <div className='attack-header'>
-            <div className='attack-icon'>
-              <BrickWallFire size={'6vw'} color='red' />
-              <h1>Red Teaming</h1>
-            </div>
-            <p style={{ margin: '0' }}>This page provides a set of vulnerabilities that can be used to test the model's robustness and a set of metrics to register the performance of each attack.</p>
+
+      <div>
+        {/* Attacks Selection */}
+        <div className='option-attacks'>
+          <div className='attack-icon'>
+            <Bug size={'3vw'} color='red' />
+            <h2>Vulnearbility selection</h2>
           </div>
-          <button className='attack-button'>
-            <Play className='icon' />
-            <div className='btn-desc'> Execute benchmark</div>
-          </button>
+          <p style={{
+            fontSize: '1.3vw',
+            color: 'gray',
+            margin: 0
+          }}>
+            Here below, are listed all the possible vulnerabilities that can be tested on the selected model.
+            For customizing a vulnearbility click on the "Settings" icon on the right. A Panel will be shown on top where all the possible customizable parameters are shown.
+          </p>
         </div>
+        <TableWrapper
+          elements={attacks}
+          selectedElement={selectedAttacks}
+          setSelectedElements={setSelectedAttacks}
+          Icon={Settings}
+        />
 
-
-        <div>
-          {/* Attacks Selection */}
-          <div className='option-attacks'>
-            <div className='attack-icon'>
-              <Bug size={'3vw'} color='red' />
-              <h2>Vulnearbility selection</h2>
-            </div>
-            <p style={{
-              fontSize: '1.3vw',
-              color: 'gray',
-              margin: 0
-            }}>
-              Here below, are listed all the possible vulnerabilities that can be tested on the selected model.
-              For customizing a vulnearbility click on the "Settings" icon on the right. A Panel will be shown on top where all the possible customizable parameters are shown.
-            </p>
+        {/* Metrics Selection */}
+        <div className='option-attacks'>
+          <div className='attack-icon'>
+            <Gauge size={'3vw'} color='red' />
+            <h2>Metric Selection</h2>
           </div>
-          <TableWrapper
-            elements={attacks}
-            selectedElement={selectedAttacks}
-            setSelectedElements={setSelectedAttacks}
-            Icon={Settings}
-          />
-
-          {/* Metrics Selection */}
-          <div className='option-attacks'>
-            <div className='attack-icon'>
-              <Gauge size={'3vw'} color='red' />
-              <h2>Metric Selection</h2>
-            </div>
-            <p style={{
-              fontSize: '1.3vw',
-              color: 'gray',
-              margin: 0
-            }}> Here it is possible to select all the metrics to measure during the vulnearbility test.</p>
-          </div>
-          <TableWrapper
-            elements={metrics}
-            selectedElement={selectedMetrics}
-            setSelectedElements={setSelectedMetrics}
-            Icon={Ruler}
-          />
+          <p style={{
+            fontSize: '1.3vw',
+            color: 'gray',
+            margin: 0
+          }}> Here it is possible to select all the metrics to measure during the vulnearbility test.</p>
         </div>
+        <TableWrapper
+          elements={metrics}
+          selectedElement={selectedMetrics}
+          setSelectedElements={setSelectedMetrics}
+          Icon={Ruler}
+        />
+      </div>
 
-      </div >);
-  }
-
-  if (error) {
-    return errorPage();
-  }
-  else {
-
-    return attackPage();
-  }
+    </div >);
 };
 
 export default Benchmark;
--- app/store/nnTrustStore.tsx
+++ app/store/nnTrustStore.tsx
@@ -1,15 +1,20 @@
-import { Job, ModelSpecs } from "@/interfaces/NNInterfaces";
+import { ModelSpecs } from "@/interfaces/NNInterfaces";
 import { create } from "zustand";
 import { createJSONStorage, persist } from "zustand/middleware";
 
 interface AppState {
   models: ModelSpecs[] | null;
   modelName: string | null;
+  attacks: Set<string>;
+  enableMonitoring: boolean      // this variable allows to tell wheter the benchmark has been executed or not
 
 
   setModels: (models: ModelSpecs[] | null) => void;
   setModelName: (modelName: string | null) => void;
+  setAttacks: (attacks: Set<string>) => void;
+  setEnableMonitoring: (enableMonitoring: boolean) => void;
 
+  updateAttacks: (attack: string) => void;
 
 }
 
@@ -18,12 +23,20 @@ const useNNTrustStore = create<AppState>()(
     (set) => ({
       models: null,
       modelName: null,
-
+      attacks: new Set(),
+      enableMonitoring: false,
 
       setModels: (models) => set({ models }),
       setModelName: (modelName) => set({ modelName }),
+      setAttacks: (attacks: Set<string>) => set({ attacks }),
+      setEnableMonitoring: (enableMonitoring: boolean) => set({ enableMonitoring }),
 
-
+      updateAttacks: (attack: string) =>
+        set((state) => {
+          const updatedAttacks = new Set(state.attacks);
+          updatedAttacks.add(attack);
+          return { attacks: updatedAttacks };
+        }),
     }),
 
     {
